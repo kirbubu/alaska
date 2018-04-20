354ab6634ce5cc62a5106a841f83de562851cde8
kirbubu <38482025+kirbubu@users.noreply.github.com>
2018-04-19 19:18:21 -0700

    graph vertex commit
    finished graph vertex class

---
+++ CECS 328 Assignment 4/CECS 328 Assignment 4/GraphVertex.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CECS_328_Assignment_4
+{
+    class GraphVertex<T>
+    {
+        //Undirected
+        //public List<GraphVertex<T>> neighbors;
+        public List<GraphVertex<T>> parents;
+        public List<GraphVertex<T>> children;
+        public T value;
+        public String symbol; 
+        public GraphVertex() {
+            value = default(T);
+            parents = new List<GraphVertex<T>>();
+            children = new List<GraphVertex<T>>();
+            symbol = "";
+        }
+
+        public GraphVertex(T value, String symbol)
+        {
+            this.value = value;
+            this.symbol = symbol;
+            parents = new List<GraphVertex<T>>();
+            children = new List<GraphVertex<T>>();
+        }
+
+        public GraphVertex(T value, String symbol, List<GraphVertex<T>> Parents, List<GraphVertex<T>> Children)
+        {
+            this.value = value;
+            this.parents = new List<GraphVertex<T>>();
+            this.children = new List<GraphVertex<T>>();
+            this.symbol = symbol;
+            if(!Equals(Parents,null))
+                foreach(GraphVertex<T> parent in Parents)
+                {
+                    //If the parent already has this vertex as a child
+                    if (parent.children.Contains(this))
+                        this.parents.Add(parent);
+                    //If the parent doesn't already have this vertex as a child
+                    else
+                    {
+                        parent.children.Add(this);
+                        this.parents.Add(parent);
+                    }
+                
+                }
+            if(!Equals(Children,null))
+                foreach(GraphVertex<T> child in Children)
+                {
+                    //If the child already has this vertex as a parent
+                    if (child.parents.Contains(this))
+                        this.children.Add(child);
+                    //If the child doesn't already have this vertex as a child
+                    else
+                    {
+                        child.parents.Add(this);
+                        this.children.Add(child);
+                    }
+                }
+            
+        }
+
+        //Checks to see if the vertex is connected
+        //i.e. is a parent or child of THIS vertex.
+        public bool IsConneceted(GraphVertex<T> Vertex)
+        {
+            if (this.parents.Contains(Vertex) || this.children.Contains(Vertex))
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
---
+++ CECS 328 Assignment 4/CECS 328 Assignment 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CECS_328_Assignment_4
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            GraphVertex<List<int>> neighbor1 = new GraphVertex<List<int>>(new List<int>(){ 1, 2 },"B");
+            GraphVertex<List<int>> testvertex = new GraphVertex<List<int>>(new List<int>() { 1, 2, 3, 4 },"A",null,new List<GraphVertex<List<int>>>() { neighbor1 });
+            Console.WriteLine(testvertex.value[3]);
+            Console.WriteLine(testvertex.children[0].value[0]);
+            
+            Debugger.Break();
+            Console.WriteLine(testvertex.children[0].parents[0].symbol); //Checking to see who A's first child is and what the child's first parent is 
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
