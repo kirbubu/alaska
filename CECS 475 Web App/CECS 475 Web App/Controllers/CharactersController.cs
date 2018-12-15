ff07ae2d9370556a9cc9352dcc9afc682e57cf1c
kirbubu <38482025+kirbubu@users.noreply.github.com>
2018-12-14 19:44:24 -0800

    Add Images Controller
    Add images controller, allowing users to view smash bros characters

---
+++ CECS 475 Web App/CECS 475 Web App/Controllers/CharactersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CECS_475_Web_App.Models;
+using System.Text.RegularExpressions;
+
+namespace CECS_475_Web_App.Controllers
+{
+    public class CharactersController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            string path = Directory.GetCurrentDirectory();
+            DirectoryInfo d = new DirectoryInfo(path + @"\wwwroot\images\Characters");
+            FileInfo[] Files = d.GetFiles("*.webp");
+            var Characters = new List<Character>();
+            foreach(var file in Files)
+            {
+                Characters.Add(new Character() { File = file.Name, Name = Regex.Replace(file.Name, @"(^\w)|(\s\w)", m => m.Value.ToUpper()).Replace(".webp","") });
+            }
+
+            return View(Characters);
+        }
+    }
+}
---
+++ CECS 475 Web App/CECS 475 Web App/Models/CharacterModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CECS_475_Web_App.Models
+{
+
+    public class CharacterModel
+    {
+
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        
+        
+        public IEnumerable<Character> Characters { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var chars = from c in Characters
+                        select c;
+            if (!String.IsNullOrEmpty(SearchString))
+            { 
+                chars = chars.Where(s => s.Name.Contains(SearchString));
+            }
+            //characters = await chars.ToAsyncList();
+
+
+        }
+
+    }
+}
